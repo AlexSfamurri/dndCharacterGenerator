1fdd156806db2fb5a44b1e510de0026c38e0913a
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-11-17 16:35:52 -0600

    set up for listening

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
-const app = require('express');
+const app = require('express')();
 const bodyParser = require('body-parser');
 const dice = require('roll');
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('we are listening on ' + port);
+});
