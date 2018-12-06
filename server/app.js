adb1a6e74e3efd53a1641456c760364cfa9a5710
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-12-06 14:28:10 -0600

    forgot to require express, glad i caught that mistake

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const app = require('express')();
+// this is where most of the server requirements are going to be made
+const app = require('express');
 const bodyParser = require('body-parser');
 const dice = require('roll');
 
